7d01683285ef677fd15f5adb06c393d15990518d
ganesan arunachalam <ganesan.arunachalam91@gmail.com>
2023-01-03 22:58:31 +0530

    Introduce DTO

--- Program.cs
+++ Program.cs
@@ -18,30 +18,34 @@ app.Run();
 
 static async Task<IResult> GetAllTodos(TodoDb db)
 {
-    return TypedResults.Ok(await db.Todos.ToArrayAsync());
+    return TypedResults.Ok(await db.Todos.Select(todo => new TodoItemDTO(todo)).ToArrayAsync());
 }
 
 static async Task<IResult> GetCompleteTodos(TodoDb db)
 {
-    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(todo => new TodoItemDTO(todo)).ToListAsync());
 }
 
 static async Task<IResult> GetTodo(int id, TodoDb db)
 {
     return await db.Todos.FindAsync(id) is Todo todo
-        ? TypedResults.Ok(todo)
+        ? TypedResults.Ok(new TodoItemDTO(todo))
         : TypedResults.NotFound();
 }
 
-static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
 {
+    var todo = new Todo {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name
+    };
     await db.Todos.AddAsync(todo);
     await db.SaveChangesAsync();
 
-    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+    return TypedResults.Created($"/todoitems/{todo.Id}", new TodoItemDTO(todo));
 }
 
-static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDto, TodoDb db)
 {
     var todo = await db.Todos.FindAsync(id);
     if (todo is null)
@@ -49,8 +53,8 @@ static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
         return TypedResults.NotFound();
     }
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDto.Name;
+    todo.IsComplete = todoItemDto.IsComplete;
 
     await db.SaveChangesAsync();
 
@@ -63,7 +67,7 @@ static async Task<IResult> DeleteTodo(int id, TodoDb db)
     {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
-        return TypedResults.Ok(todo);
+        return TypedResults.Ok(new TodoItemDTO(todo));
     }
 
     return TypedResults.NotFound();
--- Todo.cs
+++ Todo.cs
@@ -5,4 +5,12 @@ class Todo
     public string? Name { get; set; }
 
     public bool IsComplete { get; set; }
+
+    public string? Secret { get; set; }
+
+    public void Deconstruct(out int id, out string name, out bool isComplete) {
+        id = Id;
+        name = Name;
+        isComplete = IsComplete;
+    }
 }
